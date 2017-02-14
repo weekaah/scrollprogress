43b39c3913eb7efa046dfe89373d357c7110d190
Viktor Lesic <weekaah@gmail.com>
2017-02-14 11:49:25 +0100

    add scroller initialization

---
+++ js/scrollprogress.js
@@ -0,0 +1,35 @@
+(function (root, factory) {
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.Scroller = factory();
+  }
+}(this, function() {
+
+  var Scroller = function(selector) {
+    this.container = document.querySelector(selector);
+    this.progress;
+
+    this.init();
+  };
+
+  Scroller.prototype = {
+    init: function() {
+      build.call(this);
+    }
+  };
+
+  function build() {
+    this.progress = document.createElement('div');
+    this.progress.classList.add('sp-progress');
+    this.container.appendChild(this.progress);
+  }
+
+  window.scrollprogress = function(selector) {
+    return new Scroller(selector);
+  };
+
+}));
